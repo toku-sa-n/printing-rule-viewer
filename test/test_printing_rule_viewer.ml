6ee25a23a629725e34a27668f0b1241cc9475da4
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-27 15:20:27 +0900

    Remove a trivial test case

--- test/test_printing_rule_viewer.ml
+++ test/test_printing_rule_viewer.ml
@@ -4,14 +4,11 @@ let test_list_unparsing () =
     (Printing_rule_viewer.unparse "Theorem foo:1=1.")
     [ [ "Box" ] ]
 
-let test_one_equals_one () = Alcotest.(check int) "one equals one" 1 1
-
 let () =
   Alcotest.run "Printing rule viewer"
     [
       ( "test suite",
         [
-          Alcotest.test_case "Test 1 = 1" `Quick test_one_equals_one;
           Alcotest.test_case "Parsing an equality notation" `Quick
             test_list_unparsing;
         ] );
