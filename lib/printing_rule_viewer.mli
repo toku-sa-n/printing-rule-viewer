351944d96df892de7f7335db026b02d7c6198f70
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-27 15:32:36 +0900

    Add docs

--- lib/printing_rule_viewer.mli
+++ lib/printing_rule_viewer.mli
@@ -1,2 +1,8 @@
 val extract_notations_from_expressions : string -> string list
+(** Extract notations from expressions in the given Coq source code. *)
+
 val init : unit -> unit
+(** Initialize the Coq environment and the handler of command line arguments.
+
+    This function must be called once and only once, and in the very early
+    stage. *)
