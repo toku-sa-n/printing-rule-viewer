d2697e417fbe22ac92ee9823193dcd66ea257db8
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-27 15:02:03 +0900

    Add a trivial test case

--- test/test_printing_rule_viewer.ml
+++ test/test_printing_rule_viewer.ml
@@ -1,1 +1,8 @@
-let () = Alcotest.run "Printing rule viewer" []
+let test_one_equals_one () = Alcotest.(check int) "one equals one" 1 1
+
+let () =
+  Alcotest.run "Printing rule viewer"
+    [
+      ( "test suite",
+        [ Alcotest.test_case "Test 1 = 1" `Quick test_one_equals_one ] );
+    ]
