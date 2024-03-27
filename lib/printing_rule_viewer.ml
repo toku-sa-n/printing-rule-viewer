5deb2b32a0732016cea4354a93db300b8b8f8538
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-27 15:20:16 +0900

    Add a test

---
+++ lib/printing_rule_viewer.ml
@@ -0,0 +1,1 @@
+let unparse _ = [ [ "Box" ] ]
--- test/test_printing_rule_viewer.ml
+++ test/test_printing_rule_viewer.ml
@@ -1,8 +1,18 @@
+let test_list_unparsing () =
+  Alcotest.(check (list (list string)))
+    "Parsing an equality notation"
+    (Printing_rule_viewer.unparse "Theorem foo:1=1.")
+    [ [ "Box" ] ]
+
 let test_one_equals_one () = Alcotest.(check int) "one equals one" 1 1
 
 let () =
   Alcotest.run "Printing rule viewer"
     [
       ( "test suite",
-        [ Alcotest.test_case "Test 1 = 1" `Quick test_one_equals_one ] );
+        [
+          Alcotest.test_case "Test 1 = 1" `Quick test_one_equals_one;
+          Alcotest.test_case "Parsing an equality notation" `Quick
+            test_list_unparsing;
+        ] );
     ]
