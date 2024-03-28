6727d67ee93c85182c56301924716b3ec964596e
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-28 09:26:12 +0900

    Extract the notation from an AST

--- lib/view.ml
+++ lib/view.ml
@@ -1,1 +1,35 @@
-let extract_notations_from_expressions _ = [ "_ = _" ]
+open Sexplib.Sexp
+
+let rec find_sexp cond = function
+  | x when cond x -> [ x ]
+  | Atom _ -> []
+  | List xs -> List.concat_map (find_sexp cond) xs
+
+let extract_cnotations ast =
+  let cond = function
+    | List
+        [
+          List [ Atom "v"; List (Atom "CNotation" :: _) ]; List (Atom "loc" :: _);
+        ] ->
+        true
+    | _ -> false
+  in
+
+  let extract_notations = function
+    | Constrexpr.CNotation (_, (_, notation), _) -> notation
+    | _ -> failwith "unreachable"
+  in
+
+  Serlib.Ser_vernacexpr.sexp_of_vernac_control ast
+  |> find_sexp cond
+  |> List.map Serlib.Ser_constrexpr.constr_expr_of_sexp
+  |> List.map (fun x -> x.CAst.v)
+  |> List.map extract_notations
+
+let extract_notations_from_expressions code =
+  let f parser =
+    match Astparser.next parser with
+    | None -> []
+    | Some ast -> extract_cnotations ast
+  in
+  f (Astparser.make code)
